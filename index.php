1079af7bba75264a15668f660c335e65b8b9c36a
Cristian Gerardo Hernandez Barrios <34076011+Crigerprogrammer@users.noreply.github.com>
2020-05-27 00:31:21 -0600

    Inicio
    Index con info del grupo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Proyecto Ingenieria de Software</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<header>
+	<?php
+		require 'views/navbar.php';
+	?>
+</header>
+<main>
+    <center><bold><h1>Proyecto Escuela</h1></bold></center>
+    <br><br><br><br><br>
+    <center><h5>Aplicacion Desarrollada Por:</h5></center>
+    <center><h6>Renato Monterroso 0900-15-8984</h6></center>
+    <center><h6>Cristian Hernandez 0900-15-549</h6></center>
+    <center><h6>Esau Garcia 0900-15-182</h6></center>
+    <center><h6>Edgar Bolaños 0900-13-16195</h6></center>
+</main>
+<body>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/export.js"></script>
+ </body>
+</html>
